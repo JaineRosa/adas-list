67b0e4a04f9faef7ab621528701f9faf5b1126bd
Jaíne Rosa <jaine.rosa_11@live.com>
2024-03-14 19:44:51 -0300

    Feature : CRUD para a classe MainProduto

--- projetoList/src/br/com/projetoList/MainProduto.java
+++ projetoList/src/br/com/projetoList/MainProduto.java
@@ -21,62 +21,67 @@ public class MainProduto {
 			System.out.println("[2] Lista de Produtos     ");
 			System.out.println("[3] Alterar Produto     ");
 			System.out.println("[4] Excluir  Produto     ");
-			System.out.println("[0] sair");
+			System.out.println("[0] Sair");
 			System.out.println("---------------------");
 			opcao = scan.nextInt();
 
 			switch (opcao) {
 
 			case 1:
-				System.out.println("Informe o Produto para adicionar: ");
+				System.out.println("/nInforme o Produto para adicionar: ");
 				adicionar = scan.next();
 				listaProdutos.add(adicionar);
 
-				System.out.println("Produto adicionado com sucesso");
+				System.out.println("/nProduto adicionado com sucesso");
 
 				break;
 
 			case 2:
 
-				System.out.println("Lista de produtos");
+				if (listaProdutos.size() > 0) {
+					System.out.println("/nLista de produtos");
 
-				for (String nomeProduto : listaProdutos) {
-					System.out.println(nomeProduto);
+					for (String nomeProduto : listaProdutos) {
+						System.out.println(nomeProduto);
+					}
+				} else {
+					System.out.println("/nNenhum produto cadastrado ainda ...");
 				}
+				System.out.println("/n");
 
 				break;
 
 			case 3:
-				System.out.println("Informe o produto para alterar: ");
+				System.out.println("/nInforme o produto para alterar? ");
 				alterar = scan.next();
 				int indexAlterar = listaProdutos.indexOf(alterar);
 
 				if (indexAlterar >= 0) {
-					System.out.println("Informe o novo nome do produto:");
+					System.out.println("/nInforme o novo nome do produto?");
 					alterado = scan.next();
 					listaProdutos.set(indexAlterar, alterado);
 
-					System.out.println("Produto alterado com sucesso");
+					System.out.println("/nProduto alterado com sucesso");
 				} else {
-					System.out.println("Produto não encontrado");
+					System.out.println("/nProduto não encontrado");
 				}
 
 				break;
 
 			case 4:
 
-				System.out.println("Informe o nome do produto para excluir");
+				System.out.println("/nInforme o nome do produto para excluir");
 				excluir = scan.next();
 				int indexExcluir = listaProdutos.indexOf(excluir);
 				listaProdutos.remove(indexExcluir);
 
-				System.out.println("Produto excluido");
+				System.out.println("/nProduto excluido");
 
 				break;
 
-			default:
+			case 0:
 
-				System.out.println("Opção não encontrada");
+				System.out.println("/nSaindo do menu!!");
 
 			}
 
